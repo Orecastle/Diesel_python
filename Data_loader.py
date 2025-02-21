6af321c6aa20065a266e9c4fa5536f0d0c1d7034
Orecastle <malmborgjonas@gmail.com>
2025-02-21 22:46:10 +0100

    fg

--- Data_loader.py
+++ Data_loader.py
@@ -1,4 +1,11 @@
 import pandas as pd
 
 diesel_df = pd.read_csv('multivariate_diesel.csv', delimiter=';')
+
 print(diesel_df.head())
+print(diesel_df.info())
+
+CNN_df = pd.read_csv('test_scores_100.csv', delimiter=',')
+
+print(CNN_df.head())
+print(CNN_df.info())
