efed8a048500a2d51541ac6c051562ac9473c930
Orecastle <malmborgjonas@gmail.com>
2025-02-23 20:22:27 +0100

    d

--- Data_loader.py
+++ Data_loader.py
@@ -6,6 +6,7 @@ print(diesel_df.head())
 print(diesel_df.info())
 
 CNN_df = pd.read_csv('test_scores_100.csv', delimiter=',')
+CNN_df['is_same_substances?'] = CNN_df['is_same_substances?'].astype(str)
 
 print(CNN_df.head())
 print(CNN_df.info())
--- Raw_data_plotting_CNN.py
+++ Raw_data_plotting_CNN.py
@@ -1,17 +1,20 @@
-from plotnine import ggplot, aes, geom_histogram, labs, geom_density, theme_bw
-
+import numpy as np
+from plotnine import ggplot, aes, geom_density, labs, theme_bw, scale_y_continuous, scale_x_continuous
 from Data_loader import CNN_df
-
 
 print(CNN_df.info())
 
-histogram_LR = (
-    ggplot(CNN_df) +
-    geom_histogram(aes(x='LR', fill = "is_same_substances?"),
-                   binwidth=0.05, color='black',
-                   size=0.1 , alpha = 0.4)  +
-    labs(title='Histogram of LRs', x='Values', y='Frequency')+
+CNN_df_sample = CNN_df.sample(1000)  # Random sample of 1,000 rows
+CNN_df_sample['log_LR'] = np.log10(CNN_df_sample['LR'])  # Apply log10 transformation
+
+density_LR = (
+    ggplot(CNN_df_sample) +
+    geom_density(aes(x='log_LR', fill="is_same_substances?"),
+                 color='black', alpha=0.4) +
+    labs(title='Density of Log10 LRs', x='Log10(LR)', y='Density') +
+    scale_x_continuous(limits=(-10,10)) +
     theme_bw()
 )
 
-histogram_LR.show()
+density_LR.show()
+
